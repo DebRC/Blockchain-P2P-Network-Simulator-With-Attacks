f3ee1ae33420ad4e245d8c44882885ce94dcc05d
DebRC <roychowdhurydebdoot@gmail.com>
2024-02-03 18:38:55 +0530

    Changed TXN Type Val

--- lib/models/transaction.py
+++ lib/models/transaction.py
@@ -9,14 +9,14 @@ class Transaction:
     - val (float): Value associated with the transaction (e.g., amount of coins).
     - senderPeer (str): Identifier of the peer initiating the transaction or mining.
     - receiverPeer (str, optional): Identifier of the peer receiving the transaction (None for mining).
-    - txnType (int): Type of the transaction (1 for payment, 2 for mining).
+    - txnType (int): Type of the transaction (0 for payment, 1 for mining).
     - size (int): Size attribute for the transaction.
 
     Methods:
     - __init__(txnID, val, senderPeer, receiverPeer=None, txnType=1): Initializes a Transaction object.
     - __str__(): Returns a human-readable string representation of the transaction.
     """
-    def __init__(self, txnID, val, senderPeer, receiverPeer=None, txnType=1) -> None:
+    def __init__(self, txnID, val, senderPeer, receiverPeer=None, txnType=0) -> None:
         """
         Initializes a Transaction object.
         """
@@ -31,7 +31,7 @@ class Transaction:
         """
         Returns a String Representation of the Transaction object.
         """
-        if(self.type==1):
+        if(self.type==0):
             return str(self.txnID)+":"+str(self.senderPeer)+" pays "+str(self.receiverPeer)+" "+str(self.val)+" coins"
-        if(self.type==2):
+        if(self.type==1):
             return str(self.txnID)+":"+str(self.senderPeer)+" mines "+str(self.val)+" coins"
