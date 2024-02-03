68005449439fa2a44e916a15a6047448fee8e6c6
DebRC <roychowdhurydebdoot@gmail.com>
2024-02-03 14:10:24 +0530

    Updared Function

--- lib/models/block.py
+++ lib/models/block.py
@@ -1,21 +1,23 @@
-from transaction import Transaction
-from node import Node
-
 class Block:
-    def __init__(self, blockID, parentBlockID, txnList, miner, balance=[]):
+    def __init__(self, blockID, prevBlockID, txnList, prevLengthOfChain, miner, balance):
         self.blockID = blockID
-        self.parentBlockID = parentBlockID
+        self.prevBlockID = prevBlockID
         self.size = 1
         self.txnList = txnList
         self.balance = list(balance)
         self.miner = miner
+        self.length = prevLengthOfChain+1
         
         for txn in self.txnList:
             self.size+=txn.size
         
-        if self.size>1000:
-            raise ValueError(f"Size of Block Exceeded 1 MB")
+        if self.size>1024:
+            print(f"Size of Block Exceeded 1 MB")
+            return None
         
+        self.updateBalance()
+
+    def updateBalance(self):
         for txn in self.txnList:
             if txn.type == 1:
                 self.balance[txn.senderPeer.nodeID] -= txn.value
