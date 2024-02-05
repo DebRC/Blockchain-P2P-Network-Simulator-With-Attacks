7abe938fb6760bb05701a67d74265ff09ace6ca3
DebRC <roychowdhurydebdoot@gmail.com>
2024-02-05 18:53:31 +0530

    Updated comments

--- lib/models/transaction.py
+++ lib/models/transaction.py
@@ -1,4 +1,4 @@
-import json
+import json, os
 
 class Transaction:
     """
@@ -7,31 +7,34 @@ class Transaction:
     Attributes:
     - txnId (int): Unique identifier for the transaction.
     - val (float): Value associated with the transaction (e.g., amount of coins).
-    - senderPeer (str): Identifier of the peer initiating the transaction or mining.
-    - receiverPeer (str, optional): Identifier of the peer receiving the transaction (None for mining).
-    - txnType (int): Type of the transaction (0 for payment, 1 for mining).
+    - senderPeerID (int): Identifier of the peer initiating the transaction or mining (-1 for mining).
+    - receiverPeerID (int): Identifier of the peer receiving the transaction.
+    - type (int, optional): Type of the transaction (0 for payment, 1 for mining).
     - size (int): Size attribute for the transaction.
 
     Methods:
-    - __init__(txnID, val, senderPeer, receiverPeer=None, txnType=1): Initializes a Transaction object.
+    - __init__(txnID, val, senderPeerID, receiverPeerID, type=0): Initializes a Transaction object.
     - __str__(): Returns a human-readable string representation of the transaction.
     """
-    def __init__(self, txnID, val, senderPeer, receiverPeer=None, txnType=0) -> None:
+    def __init__(self, txnID, val, senderPeerID, receiverPeerID, type=0) -> None:
         """
         Initializes a Transaction object.
         """
         self.txnID = txnID
         self.val = val        
-        self.senderPeer = senderPeer
-        self.receiverPeer = receiverPeer
-        self.type = txnType
-        self.size = json.load(open('params.json'))['txn-size']
+        self.senderPeerID = senderPeerID
+        self.receiverPeerID = receiverPeerID
+        self.type = type
+
+        script_dir = os.path.dirname(os.path.abspath(__file__))
+        params_path = os.path.join(script_dir,"../helper",'params.json')
+        self.size = json.load(open(params_path))['txn-size']
         
     def __str__(self) -> str:
         """
         Returns a String Representation of the Transaction object.
         """
         if(self.type==0):
-            return str(self.txnID)+":"+str(self.senderPeer)+" pays "+str(self.receiverPeer)+" "+str(self.val)+" coins"
+            return str(self.txnID)+":"+str(self.senderPeerID)+" pays "+str(self.receiverPeerID)+" "+str(self.val)+" coins"
         if(self.type==1):
-            return str(self.txnID)+":"+str(self.senderPeer)+" mines "+str(self.val)+" coins"
+            return str(self.txnID)+":"+str(self.receiverPeerID)+" mines "+str(self.val)+" coins"
